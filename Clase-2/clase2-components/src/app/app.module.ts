0fc5e332f8542ca64a9e8dfaf5b2cc283c4634f4
martinEscalante01 <escalantejuanmartin00@gmail.comm>
2025-03-17 23:01:35 -0300

    clase-2

---
+++ Clase-2/clase2-components/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { StudentComponent } from './components/student/student.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    ToolbarComponent,
+    StudentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
